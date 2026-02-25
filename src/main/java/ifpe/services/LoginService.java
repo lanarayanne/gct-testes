ace50e45152d36dd2a893bfc73e65dab41a0936c
Lana <alana.rayanne@gmail.com>
2026-02-25 19:22:11 -0300

    teste realizar Login com email errado (fix #1)

--- src/main/java/ifpe/services/LoginService.java
+++ src/main/java/ifpe/services/LoginService.java
@@ -1,24 +1,26 @@
 package ifpe.services;
 
 import ifpe.entidades.Login;
+import ifpe.entidades.Usuario;
 import ifpe.repositorios.LoginRepositorio;
+import ifpe.repositorios.UsuarioRepositorio;
 
 public class LoginService {
     private LoginRepositorio loginRepositorio;
+    private UsuarioRepositorio usuarioRepositorio;
 
-    public LoginService(LoginRepositorio repo){
-        this.loginRepositorio = repo;
+    public LoginService(LoginRepositorio loginRepo, UsuarioRepositorio usuarioRepo){
+        this.loginRepositorio = loginRepo;
+        this.usuarioRepositorio=usuarioRepo;
     }
 
     public Login fazerLogin(Login login) {
-        if (!login.getEmail().contains("@") || !login.getEmail().contains(".com")) {
-            throw new IllegalArgumentException("E-mail inválido");
+        String email = login.getEmail();
+        Usuario usuario = this.usuarioRepositorio.buscarPorEmail(email);
+        if(usuario == null){
+            throw new NullPointerException ("Usuário não encontrado");
         }
-        String senha = login.getSenha();
-        String regexComplexa = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*]).*$";
-        if (senha == null || !senha.matches(regexComplexa)) {
-            return null;
-        }
+
         return this.loginRepositorio.fazerLogin(login);
     }
 }
